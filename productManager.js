570edc48dc0f2b573fe6d76a9078189805c83694
lolazo455 <mati.a.gimenez2002@gmail.com>
2023-02-04 17:53:33 -0300

    segunda entrega backend

--- productManager.js
+++ productManager.js
@@ -1,22 +1,44 @@
 
-
+const {saveArchive, readArchive} = require("./fileSystemManager")
 //Desafio entregable 1
 
 class ProductManager {
     static idCounter = 0;
     static productProperties = ["title", "description", "price", "thumbnail", "code", "stock"]
-    constructor(){
+    constructor(path){
+        this.path = path
         this.products = []
     }
 
-    addProduct = (productToAdd) => {
-        let results = []
-        for(const property of ProductManager.productProperties){
-            results.push(Object.keys(productToAdd).includes(property))
+    updateProduct = async (productToUpdate) =>{
+        const indice = this.products.findIndex(product => Number(product.id) === Number(productToUpdate.id))
+        if(indice === -1){
+            return {"error": "The product´s id provided incorrect"}
+        }else{
+            if(this.hasAllProperties(productToUpdate)){
+                this.products[indice] =  productToUpdate
+                await saveArchive(this.path, this.products)
+                return this.products
+            }else{
+                return {"error": `Error: the product must have ${ProductManager.productProperties.join(", ")}`}
+            }
+            
         }
-        if(results.every(result => result)){
+    }
+    hasAllProperties = (productToCheck) =>{
+        const results = []
+        for(const property of ProductManager.productProperties){
+            results.push(Object.keys(productToCheck).includes(property))
+        }
+        return results.every(result => result)
+    }
+    addProduct = async (productToAdd) => {
+        
+        if(this.hasAllProperties(productToAdd)){
             if(!this.products.some(product => product.code === productToAdd.code)){
                 this.products.push({...productToAdd, id: ProductManager.idCounter++})
+                await saveArchive(this.path, this.products)
+                return await readArchive(this.path).then(res => res)
             }else{
                 return "Error: you cannot repeat the code field"
             }
@@ -26,30 +48,29 @@ class ProductManager {
         
     }
     
-    getProducts = () => this.products
+    getProducts = async () => await readArchive(this.path)
 
-    getProductsById = (id) => {
-        if(this.products.some(product => product.id === id)){
-            return this.products.find(product => product.id === id )
+    getProductsById =  (id) => {
+        const product = this.products.find(product => product.id === id )
+        if(product){
+            return product
         }else{
-            return "Error: product not found"
+            return "Error: Product not found"
         }
+    }
+    deleteById = async (id) =>{
+        this.products = this.products.filter(product => Number(product.id) !== Number(id))
+        await saveArchive(this.path,this.products)
+        return this.product
     }
 }
 
-const manager = new ProductManager()
+const manager = new ProductManager("./db/products.json")
 
-console.log(manager.addProduct({title: "manzana",description: "lorem ipsum dolor", price: 1223, thumbnail: "...", stock: 4, code: "AA01"}))
-console.log(manager.addProduct({title: "manzana",description: "lorem ipsum dolor", price: 1223, thumbnail: "...", stock: 4, code: "AA01"}))
-console.log(manager.addProduct({title: "banana",description: "lorem ipsum dolor", price: 1222, thumbnail: "...", stock: 23, code: "AA02"}))
-console.log(manager.addProduct({title: "banana", price: 1222, thumbnail: "...", stock: 23, code: "AA02"}))
+manager.addProduct({title: "manzana",description: "lorem ipsum dolor", price: 1223, thumbnail: "...", stock: 4, code: "AA01"}).then(res => console.log(res))
+manager.addProduct({title: "manzana",description: "lorem ipsum dolor", price: 1223, thumbnail: "...", stock: 4, code: "AA01"}).then(res => console.log(res))
+manager.addProduct({title: "manzana",description: "lorem ipsum dolor", price: 1223, thumbnail: "...", stock: 4, code: "AA03"}).then(res => console.log(res))
+manager.addProduct({title: "manzana",description: "lorem ipsum dolor", price: 1223, thumbnail: "...", stock: 4, code: "AA04"}).then(res => console.log(res))
+manager.addProduct({title: "banana",description: "lorem ipsum dolor", price: 1222, thumbnail: "...", stock: 23, code: "AA02"}).then(res => console.log(res))
+manager.addProduct({title: "banana", price: 1222, thumbnail: "...", stock: 23, code: "AA02"}).then(res => console.log(res))
 
-//Tests 
-
-console.log(manager.products)
-
-console.log(manager.getProductsById(1))
-
-console.log(manager.getProductsById(7))
-
-console.log(manager.products)
